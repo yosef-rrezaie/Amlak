52260f5f8cfc35c99e54be9877b65cb27b7390d0
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-04-13 17:48:19 +0330

    develope section profiles

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -7,5 +7,5 @@ export default async function page() {
   const data = await res.json();
   console.log(data);
   if (data.error) return <h3>یک مشکلی پیش آمده است</h3>;
-  return <BuyResidentialPage data={data} />;
+  return <BuyResidentialPage data={data.data} />;
 }
--- src/components/templates/BuyResidentialPage.js
+++ src/components/templates/BuyResidentialPage.js
@@ -1,6 +1,13 @@
+import Card from "../modules/Card"
 import styles from "./BuyResidentialPage.module.css"
-export default function BuyResidentialPage() {
+export default function BuyResidentialPage({data}) {
   return (
-    <div>h</div>
+    <div className={styles.container}>
+      <div className={styles.sidebar}></div>
+      <div className={styles.main}>
+        {data.length ? null : (<p className={styles.text}>هیچ آگهی ثبت نشده است</p>)}
+        {data?.map(profile => (<Card key={profile._id} data={profile}></Card>))}
+      </div>
+    </div>
   )
 }
