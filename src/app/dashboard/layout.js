efc7f39dde7f13aa613ec6b3faab0343295b4a41
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-04-14 20:13:08 +0330

    change sidebar dashboard for ADMIN role

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,9 +2,14 @@ import DashboardSidebar from "@/components/layout/DashboardSidebar";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
+import connectDB from "@/utils/connectDB";
+import { User } from "@/models/User";
 
-export default async function  DashboardLayout({ children }) {
-    const session = await getServerSession(authOptions);
-      if (!session) redirect("/");
-  return <DashboardSidebar>{children}</DashboardSidebar>;
+export default async function DashboardLayout({ children }) {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/");
+
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+  return <DashboardSidebar role={user.role}>{children}</DashboardSidebar>;
 }
--- src/components/layout/DashboardSidebar.js
+++ src/components/layout/DashboardSidebar.js
@@ -5,17 +5,19 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Link from "next/link";
 import LogoutButton from "../modules/LogoutButton";
 import { redirect } from "next/navigation";
-export default async function DashboardSidebar({ children }) {
+export default async function DashboardSidebar({ children, role }) {
   const session = await getServerSession(authOptions);
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
+        {role === "ADMIN" ? "مدیر" : "کاربر"}
         <p>{session?.user?.email}</p>
         <span></span>
         <Link href="/dashboard/">حساب کاربری</Link>
         <Link href="/dashboard/my-profile">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
+        {role==="ADMIN" ? <Link href="/">در انتظار تایید</Link> : null }
         <LogoutButton />
       </div>
       <div className={styles.main}>{children}</div>
